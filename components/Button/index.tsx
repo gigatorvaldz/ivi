6669c609805ce669179dd5dd54248ae8b1da2a45
Istiniel <eclipsio@mail.ru>
2023-04-12 19:41:59 +0300

    fix: fix components folder

--- components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = () => {
-  return <div>123</div>;
-};
-
-export default Button;
--- components/DropDown/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DropDown = () => {
-  return <div>123</div>;
-};
-
-export default DropDown;
