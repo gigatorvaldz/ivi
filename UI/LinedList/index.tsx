06a4df279916f585e7f57a4d9e4160a94aea9413
Artyom <ltorvaldz@mail.ru>
2023-04-21 22:00:48 +0300

    fix: Lined List render fix

--- UI/LinedList/index.tsx
+++ UI/LinedList/index.tsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './LinedList.module.scss';
 import Link from 'next/link';
 import { ListItem } from '@/interfaces';
 
 interface IList {
   items: ListItem[];
+  init?: number;
 }
 
-const LinedList: React.FC<IList> = ({ items }) => {
-  const [selectedItem, setSelectedItem] = useState(0);
-  const currentItmeHeight = 25;
+const LinedList: React.FC<IList> = ({ items, init = 0 }) => {
+  const [selectedItem, setSelectedItem] = useState<number>(init);
+  const currentItemHeight = 25;
 
+  useEffect(() => {
+    setSelectedItem(init);
+  }, [items]);
+  
   return (
     <div className={css.wrapper}>
       <div className={css.line}>
         <div
-          style={{ marginTop: selectedItem * currentItmeHeight + 'px' }}
+          style={{ marginTop: selectedItem * currentItemHeight + 'px' }}
           className={css.selector}
         ></div>
       </div>
