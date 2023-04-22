98ce26eec42ae282f0fece10bc1b3973ed70317f
Artyom <ltorvaldz@mail.ru>
2023-04-22 23:33:48 +0300

    feat: add Mobile Option to Action Card

--- UI/ActionCard/index.tsx
+++ UI/ActionCard/index.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
 import css from './ActionCard.module.scss';
+import classNames from 'classnames';
+
+export type ActionCardStylingT = 'default' | 'mobile';
 
 interface ActionCardI {
   active?: boolean;
@@ -8,11 +11,19 @@ interface ActionCardI {
   label: string;
   description?: string;
   href: string;
+  styling?: ActionCardStylingT;
 }
 
-const ActionCard: React.FC<ActionCardI> = ({ href, icon, label, description, active = false }) => {
+const ActionCard: React.FC<ActionCardI> = ({
+  href,
+  icon,
+  label,
+  description,
+  active = false,
+  styling = 'default',
+}) => {
   return (
-    <Link className={css.container} href={href}>
+    <Link className={classNames(css[styling], css.container)} href={href}>
       <div className={css.icon}>{icon}</div>
       <h3>{label}</h3>
       {description && <p className={css.description}>{description}</p>}
