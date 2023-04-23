17f827c73e5bcca4dc5dc78e26c93a17e1bd5e4c
Artyom <ltorvaldz@mail.ru>
2023-04-24 01:12:23 +0300

    feat: add Drawer Component

--- components/Drawer/index.tsx
+++ components/Drawer/index.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import css from './Drawer.module.scss';
-import ActionCard from '@/UI/ActionCard';
-import { IoDiamondOutline } from 'react-icons/io5';
-import { BsAward } from 'react-icons/bs';
+import classNames from 'classnames';
+
 import Accordion from '@/UI/Accordion';
-import List from '@/UI/List';
 import Link from 'next/link';
-import { Film } from '@/constants/header';
+import ActionCard from '@/UI/ActionCard';
+import DrawerNavigationAccordion from './DrawerAccordionContent/DrawerNavigationAccordion';
+
+import { IoDiamondOutline } from 'react-icons/io5';
+import { BsAward, BsCameraReels, BsCollectionPlay, BsTv } from 'react-icons/bs';
+import { WiTrain } from 'react-icons/wi';
+
+import { Film, Series, Animation } from '@/constants/header';
+import DrawerTVAccordion from './DrawerAccordionContent/DrawerTVAccordion';
 
 const Drawer: React.FC = () => {
   useEffect(() => {
     document.body.classList.add('drawer-opened');
+
     return () => {
       document.body.classList.remove('drawer-opened');
     };
@@ -19,33 +26,76 @@ const Drawer: React.FC = () => {
   return (
     <div className={css.container}>
       <div className={css.inner}>
-        <div className={css.subscription}>
+        <div className={classNames(css['navigation-group'], css.subscription)}>
           <ActionCard
             icon={<IoDiamondOutline size={22} />}
-            label="Подписки"
+            label="Подключить подписку"
             active
             styling="mobile"
-            href="/profile/purchases"
+            href="/subscribe?from=top_menu&redirect_url=%2Fwatch%2Fdruzya"
           />
+
           <ActionCard
             icon={<BsAward size={22} />}
-            label="Покупки"
-            href="/profile/purchases"
+            label="Активация сертификата"
+            href="/cert"
             styling="mobile"
           />
         </div>
-        <div className={css.accordion}>
-          <Accordion label="Accrodion" icon={<IoDiamondOutline />}>
-            <div className={css.accordionContainer}>
-              <div className={css.title}>
-                <Link href="/">Все фильмы</Link>
-              </div>
-              <h3>Жанры</h3>
-              <div className={css.list}>
-                <List items={[...Film.genres[0], ...Film.genres[1]]} />
-              </div>
-            </div>
-          </Accordion>
+        <div className={classNames(css['navigation-group'], css['navigation-list'])}>
+          <Link href="/">
+            <h3>Мой Иви</h3>
+          </Link>
+
+          <Link href="/new">
+            <h3>Что нового</h3>
+          </Link>
+
+          <div className={css.accordion}>
+            <Accordion label="Фильмы" icon={<BsCameraReels size={22} />}>
+              <DrawerNavigationAccordion
+                genres={[...Film.genres[0], ...Film.genres[1]]}
+                years={Film.years}
+                countries={Film.countries}
+                widgets={Film.line}
+                header={{ title: 'Все фильмы', href: '/movies' }}
+              />
+            </Accordion>
+          </div>
+
+          <div className={css.accordion}>
+            <Accordion label="Сериалы" icon={<BsCollectionPlay size={22} />}>
+              <DrawerNavigationAccordion
+                genres={[...Series.genres[0], ...Series.genres[1]]}
+                years={Series.years}
+                countries={Series.countries}
+                widgets={Series.line}
+                header={{ title: 'Все сериалы', href: '/series' }}
+              />
+            </Accordion>
+          </div>
+
+          <div className={css.accordion}>
+            <Accordion label="Мультфильмы" icon={<WiTrain size={30} />}>
+              <DrawerNavigationAccordion
+                genres={[...Animation.genres[0], ...Animation.genres[1]]}
+                years={Animation.years}
+                countries={Animation.countries}
+                widgets={Animation.line}
+                header={{ title: 'Все мультфильмы', href: '/animation' }}
+              />
+            </Accordion>
+          </div>
+
+          <div className={css.accordion}>
+            <Accordion label="TV+" icon={<BsTv size={22} />}>
+              <DrawerTVAccordion />
+            </Accordion>
+          </div>
+
+          <Link href="/goodmovies">
+            <h3>Что посмотреть</h3>
+          </Link>
         </div>
       </div>
     </div>
