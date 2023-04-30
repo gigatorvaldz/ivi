62917bd498b2439f9b2e827cff7bc3dbb6d1e850
kyzne <kyznetsovstepa@gmail.com>
2023-04-30 17:14:31 +0300

    feat: add adaptive to some components

--- hooks/useMediaQueries.tsx
+++ hooks/useMediaQueries.tsx
@@ -2,7 +2,7 @@ import { useMediaQuery } from 'react-responsive';
 
 export const useMediaQueriesMinWidth = () => {
   const isDesktop = useMediaQuery({ query: '(min-width: 1280px)' });
-  const isLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
+  const isLaptop = useMediaQuery({ query: '(min-width: 1160px)' });
   const isTable = useMediaQuery({ query: '(min-width: 768px)' });
   const is500 = useMediaQuery({ query: '(min-width: 500px)' });
   const isMobile = useMediaQuery({ query: '(min-width: 320px)' });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Footer from '@/components/Footer';
-import css from '/styles/Home.module.scss';
 import { NextPage } from 'next';
+import css from '/styles/Home.module.scss';
 import Banner from '@/components/Banner';
 import { bannerSlides } from '@/constants/bannerSlides';
 import BannerSlide from '@/UI/BannerSlide';
@@ -10,7 +9,6 @@ import { FilmCardArray } from '@/mocks/FilmCardArray';
 import { topTenCards } from '@/constants/topTenCards';
 import TopTenCard from '@/UI/TopTenCard';
 import titleBadge from '/assets/topTen/titleBadge.svg';
-import Header from '@/components/Header';
 import DescriptionBlock from '@/components/DescriptionBlock';
 import { description } from '@/constants/mainPageDescriptionBlock';
 import TeaserList from '@/UI/TeaserList';
@@ -18,74 +16,70 @@ import lightning from '/assets/mainPage/lightning.svg';
 import gift from '/assets/mainPage/gift.svg';
 import { collections } from '@/constants/collections';
 import CollectionItem from '@/UI/CollectionItem';
+import Layout from '@/components/Layout';
 
 const Home: NextPage = () => {
   return (
     <>
       <Meta title="Главная" description="Онлайн кинотеатр Иви" />
-      <Header />
-      <main className={css.main}>
-        <Banner
-          slides={bannerSlides.map((e, index) => (
-            <BannerSlide
-              key={index}
-              backgroundImage={e.backgroundImage}
-              title={e.title}
-              info={e.info}
-              buttonText={e.buttonText}
-              href={e.href}
-              textColor={e.textColor}
-            />
-          ))}
-        />
-        <section>
-          <div className="wrapper">
-            <TeaserList
-              items={[
-                { text: '30 дней подписки за 1 ₽', icon: lightning },
-                { text: 'Активировать сертификат', icon: gift },
-              ]}
-            />
-          </div>
-        </section>
-        <Galery 
-          isTitleLink={false}
-          title='Подборки'
-          slides={collections.map((e, index) => (
-            <CollectionItem 
-              key={index}
-              title={e.title}
-              image={e.image}
-              href={e.href}
-            />
-          ))}
-        />
-        <Galery
-          titleImage={titleBadge}
-          title="за неделю"
-          slides={topTenCards.map((e, index) => (
-            <TopTenCard
-              key={index}
-              position={e.position}
-              title={e.title}
-              poster={e.poster}
-              href={e.href}
-            />
-          ))}
-        />
-        <section className={css.description}>
-          <div className="wrapper">
-            <DescriptionBlock
-              isMainPage
-              title="Онлайн-кинотеатр Иви: фильмы в хорошем качестве всегда приносят настоящее удовольствие"
-              content={description}
-            />
-          </div>
-        </section>
-        <Galery title="Добрые мультфильмы" slides={FilmCardArray} />
-        <Galery title="Драмы" slides={FilmCardArray} />
-      </main>
-      <Footer />
+      <Layout>
+        <main className={css.main}>
+          <Banner
+            slides={bannerSlides.map((e, index) => (
+              <BannerSlide
+                key={index}
+                backgroundImage={e.backgroundImage}
+                title={e.title}
+                info={e.info}
+                buttonText={e.buttonText}
+                href={e.href}
+                textColor={e.textColor}
+              />
+            ))}
+          />
+          <section>
+            <div className="wrapper">
+              <TeaserList
+                items={[
+                  { text: '30 дней подписки за 1 ₽', icon: lightning },
+                  { text: 'Активировать сертификат', icon: gift },
+                ]}
+              />
+            </div>
+          </section>
+          <Galery
+            isTitleLink={false}
+            title="Подборки"
+            slides={collections.map((e, index) => (
+              <CollectionItem key={index} title={e.title} image={e.image} href={e.href} />
+            ))}
+          />
+          <Galery
+            titleImage={titleBadge}
+            title="за неделю"
+            slides={topTenCards.map((e, index) => (
+              <TopTenCard
+                key={index}
+                position={e.position}
+                title={e.title}
+                poster={e.poster}
+                href={e.href}
+              />
+            ))}
+          />
+          <section className={css.description}>
+            <div className="wrapper">
+              <DescriptionBlock
+                isMainPage
+                title="Онлайн-кинотеатр Иви: фильмы в хорошем качестве всегда приносят настоящее удовольствие"
+                content={description}
+              />
+            </div>
+          </section>
+          <Galery title="Добрые мультфильмы" slides={FilmCardArray} />
+          <Galery title="Драмы" slides={FilmCardArray} />
+        </main>
+      </Layout>
     </>
   );
 };
