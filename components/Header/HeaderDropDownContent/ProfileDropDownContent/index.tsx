4ffb1f9f06a1aaa1410a6b78dffa81b74f495343
Artyom <ltorvaldz@mail.ru>
2023-05-05 00:24:42 +0300

    feat: add Auth Button to Header-component

--- components/Header/HeaderDropDownContent/ProfileDropDownContent/index.tsx
+++ components/Header/HeaderDropDownContent/ProfileDropDownContent/index.tsx
@@ -16,6 +16,7 @@ import {
 } from 'react-icons/bs';
 import { IoDiamondOutline } from 'react-icons/io5';
 import { TbDeviceTvOld } from 'react-icons/tb';
+import { useRouter } from 'next/router';
 
 const controlsItems: Array<ListItem> = [
   { title: 'Настройки', href: '/profile/settings' },
@@ -26,39 +27,51 @@ const controlsItems: Array<ListItem> = [
 ];
 
 const ProfileDropDownContent: React.FC = () => {
+  const router = useRouter();
+
+  // Auth Buttons
+  const onAuthButtonClickHandle = (type: string) => {
+    return () => {
+      router.push(router.asPath + `?${type}`, undefined, { shallow: true });
+    };
+  };
+
   return (
     <div className={css.container}>
-        <div className={css.actions}>
-          <ActionCard
-            icon={<BsCollectionPlay size={22} />}
-            label="Покупки"
-            href="/profile/purchases"
-          />
-          <ActionCard
-            icon={<BsBookmark size={22} />}
-            label="Смотреть позже"
-            href="/profile/purchases"
-          />
-          <ActionCard
-            icon={<BsClockHistory size={22} />}
-            label="История просмотров"
-            href="/profile/purchases"
-          />
-          <ActionCard
-            icon={<IoDiamondOutline size={22} />}
-            label="Подписки"
-            description="Подключить"
-            active
-            href="/profile/purchases"
-          />
-          <ActionCard icon={<BsAward size={22} />} label="Покупки" href="/profile/purchases" />
-          <ActionCard icon={<TbDeviceTvOld size={22} />} label="Покупки" href="/profile/purchases" />
-          <ActionCard icon={<BsCreditCard size={22} />} label="Покупки" href="/profile/purchases" />
+      <div className={css.actions}>
+        <ActionCard
+          icon={<BsCollectionPlay size={22} />}
+          label="Покупки"
+          href="/profile/purchases"
+        />
+        <ActionCard
+          icon={<BsBookmark size={22} />}
+          label="Смотреть позже"
+          href="/profile/purchases"
+        />
+        <ActionCard
+          icon={<BsClockHistory size={22} />}
+          label="История просмотров"
+          href="/profile/purchases"
+        />
+        <ActionCard
+          icon={<IoDiamondOutline size={22} />}
+          label="Подписки"
+          description="Подключить"
+          active
+          href="/profile/purchases"
+        />
+        <ActionCard icon={<BsAward size={22} />} label="Покупки" href="/profile/purchases" />
+        <ActionCard icon={<TbDeviceTvOld size={22} />} label="Покупки" href="/profile/purchases" />
+        <ActionCard icon={<BsCreditCard size={22} />} label="Покупки" href="/profile/purchases" />
+      </div>
+      <div className={css.controls}>
+        <div className={css.auth}>
+          <Button primaryText="Войти" styling="accent" />
+          <Button primaryText="Зарегистрироваться" styling="accent" />
         </div>
-        <div className={css.controls}>
-          <Button primaryText="Войти или зарегистрироваться" styling="accent" />
-          <List items={controlsItems} />
-        </div>
+        <List items={controlsItems} />
+      </div>
     </div>
   );
 };
