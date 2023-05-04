3897729f1bd770075632e6b18581ae7a3bc6f986
Artyom <ltorvaldz@mail.ru>
2023-05-04 20:53:33 +0300

    feat: locales destructing

---
+++ locales/Ar.ts
@@ -0,0 +1,5 @@
+export const en = {
+}
+
+export const ru = {
+}
--- locales/en.ts
+++ locales/en.ts
@@ -1,3 +1,10 @@
+import { en as Ar } from "./Ar"
+import { en as Ed } from "./Ed"
+import { en as St } from "./St"
+
 export const en = {
+    ...Ar,
+    ...Ed,
+    ...St,
     search: "Search"
 };
