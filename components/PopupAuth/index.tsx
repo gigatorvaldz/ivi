050ee5b83afe8eaf4a36f46b96740d0cfc184d73
Artyom <ltorvaldz@mail.ru>
2023-05-05 00:23:54 +0300

    refactor: Popup Auth refactoring

--- components/PopupAuth/index.tsx
+++ components/PopupAuth/index.tsx
@@ -4,29 +4,34 @@ import css from './PopupAuth.module.scss';
 import router from 'next/router';
 import { IoCloseOutline } from 'react-icons/io5';
 import Message from '@/UI/Message';
-import AuthForm from './AuthForm';
+import AuthForm from './SignUpForm';
+import SignInForm from './SignInForm';
+
+interface PopupAuthI {
+  type: 'registration' | 'login';
+}
 
 const closeButtonHandle = () => {
   router.back();
 };
 
-const PopupAuth: React.FC = () => {
+const PopupAuth: React.FC<PopupAuthI> = ({ type }) => {
   return (
     <Modal visible={true}>
       <div className={css.container}>
         <div className={css.header}>
-          <h2 className={css.title}>Вход или регистрация</h2>
+          <h2 className={css.title}>{type === 'registration' ? "Регистрация" : "Вход"}</h2>
           <IoCloseOutline className={css.closePopup} onClick={closeButtonHandle} />
         </div>
         <div className={css.content}>
           <div className={css.inner}>
             <div className={css.message}>
               <Message
-                title="Войдите или зарегистрируйтесь"
+                title={type === 'registration' ? "Зарегистрируйтесь" : "Войдите"}
                 extra="чтобы пользоваться сервисом на любом устройстве"
               />
             </div>
-            <AuthForm />
+            {type === 'registration' ? <AuthForm /> : <SignInForm />}
           </div>
         </div>
       </div>
